6b3749cd3733b1426f5f146f703a6134a2b603ae
bogdanka <bolotnik.andrey@mail.ru>
2019-02-24 16:01:23 +0300

    Result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0 || str.length == 0) return false;
+    let stack = new Stack();
+    for (let i = 0; i < str.length; i++) {
+        const currentSymbol = str[i];
+        const lastAddedBracket = stack.get();
+        for (let j = 0, n = bracketsConfig.length; j < n; j++) {
+            const openingBracket = bracketsConfig[j][0];
+            const closingBracket = bracketsConfig[j][1];
+            if (lastAddedBracket == openingBracket && currentSymbol == closingBracket)
+                stack.delete();
+            else if (currentSymbol == openingBracket)
+                stack.add(currentSymbol);
+        }
+    }
+    return stack.isEmpty();
 }
+
+class Stack {
+
+
+    constructor() {
+        this.arr = [];
+    }
+
+    add(number) {
+        this.arr.push(number);
+    }
+
+    get() {
+        return this.arr[this.arr.length - 1];
+    }
+
+    delete() {
+        this.arr.pop();
+    }
+
+    isEmpty() {
+        return this.arr.length === 0;
+    }
+}
+
+
+
+
